6518b5bb912dc4f18bacf8c81dd63ad8f4ae07f4
0legovich <romq1@mail.ru>
2017-08-02 14:53:39 +0300

    refractor code

--- lib/clothes.rb
+++ lib/clothes.rb
@@ -5,28 +5,42 @@ class Clothes
     @all_items = get_form_dir(path_for_data)
   end
 
-  #answer - Integer
-  def generate(answer)
-    generated_clothes = []
-    @all_items.each do |item|
-      generated_clothes << item if answer.between?(item.temperature_min, item.temperature_max)
-    end
-    kit(generated_clothes)
+  def types_items
+    @all_items.map(&:type).uniq
   end
+
+  def all_items_of_params(type, temperature)
+    @all_items.select do |item|
+      item.type == type && temperature.between?(item.temperature_min, item.temperature_max)
+    end
+  end
+
+  def generate(temperature)
+    types_items.map {|type| all_items_of_params(type, temperature).sample}.compact
+  end
+
+  #answer - Integer
+  # def generate(answer)
+  #   generated_clothes = []
+  #   @all_items.each do |item|
+  #     generated_clothes << item if answer.between?(item.temperature_min, item.temperature_max)
+  #   end
+  #   kit(generated_clothes)
+  # end
 
   private
 
   def get_form_dir(path_for_data)
     if Dir.exist?(path_for_data)
-      Dir[path_for_data + '/*.txt'].map { |item| Item.new(item) }
+      Dir[path_for_data + '/*.txt'].map {|item| Item.new(item)}
     else
       raise "Данных о вещах не найдено."
     end
   end
 
   #generated_clothes - массив элементов Item
-  def kit(generated_clothes)
-    types = generated_clothes.map { |item| item.type }.uniq
-    types.map { |type| generated_clothes.select { |item| item.type == type}.sample }
-  end
+  # def kit(generated_clothes)
+  #   types =
+  #     types.map {|type| generated_clothes.select {|item| item.type == type}.sample}
+  # end
 end
