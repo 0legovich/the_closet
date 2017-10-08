19bebf6e03e038c6d5be0bc27dd8548807a21836
0legovich <romq1@mail.ru>
2017-10-08 15:23:05 +0300

    refactor code

--- lib/wardrobe.rb
+++ lib/wardrobe.rb
@@ -25,15 +25,6 @@ class Wardrobe
     end.compact
   end
 
-  #answer - Integer
-  # def generate(answer)
-  #   generated_clothes = []
-  #   @all_items.each do |item|
-  #     generated_clothes << item if answer.between?(item.temperature_min, item.temperature_max)
-  #   end
-  #   kit(generated_clothes)
-  # end
-
   private
 
   def get_form_dir(path_for_data)
@@ -43,10 +34,4 @@ class Wardrobe
       raise "Данных о вещах не найдено."
     end
   end
-
-  #generated_clothes - массив элементов Item
-  # def kit(generated_clothes)
-  #   types =
-  #     types.map {|type| generated_clothes.select {|item| item.type == type}.sample}
-  # end
 end
